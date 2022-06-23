d5589b9a78b6f9ca7e0e82feb72bb5b93f755aae
05th <qatsby@yandex.com>
2022-06-22 20:46:53 -0400

    Cganged

--- Converter.java
+++ Converter.java
@@ -1,3 +1,6 @@
 public class Converter {
+    public static void main(String[] args) {
+        System.out.println("I am gonna push it on GIT");
+    }
 
 }
---
+++ Praktikum.java
@@ -0,0 +1,6 @@
+public class Praktikum {
+    int age;
+    String name;
+    int weigth;
+    String nation;
+}
